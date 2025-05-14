11b7fbb2ccc77f031b2a7db00ccdc801c6e5e980
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-05-13 22:58:12 -0300

    agregue acentos a las palabras del alert

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const adivinarNumero = (e)=>{
+    e.preventDefault();
+    const numero = document.getElementById('number').value
+    if(numero==randomNumber){alert('¡Acertaste!')}
+    else if(numero>randomNumber){alert('Te pasaste, es mas bajo.')}
+    else if(numero<randomNumber){alert('Te faltó, es mas alto.')}
+}
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+const formulario = document.getElementById('miFormulario')
+
+formulario.addEventListener("submit",adivinarNumero)
